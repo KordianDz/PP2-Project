09a7aa094725b06d7a0bd908ad754af5785254ba
Kordian <kdziedzic248@gmail.com>
2026-04-14 00:21:55 +0200

    Date and time major update (not completed)

--- main.c
+++ main.c
@@ -45,7 +45,9 @@ typedef struct Wypozyczenie
     int numer_karty;
     int id_sprzetu;
     Data data_wypozyczenia;
-    Data data_oddania;
+    Data przewidywana_data_oddania;
+    Data realna_data_oddania;
+    double oplacona_kwota;
     struct Wypozyczenie *next;
 } Wypozyczenie;
 
@@ -149,7 +151,7 @@ Klient *wczytaj_baze()
     return head;
 }
 
-void dodaj_wypozyczenie(Klient **h_klienci, Sprzet **h_sprzet)
+void dodaj_wypozyczenie(Klient **h_klienci, Sprzet **h_sprzet, Wypozyczenie **h_wypozyczenie)
 {
     int szukana_karta, szukany_sprzet;
     Klient *obecny_klient = *h_klienci;
@@ -187,10 +189,8 @@ void dodaj_wypozyczenie(Klient **h_klienci, Sprzet **h_sprzet)
     nowe_wypozyczenie -> numer_karty = szukana_karta;
     nowe_wypozyczenie -> id_sprzetu = szukany_sprzet;
     obecny_sprzet -> liczba_wypozyczonych ++;
-    nowe_wypozyczenie -> next = h_wypozyczenia;
-
-    
-
+    nowe_wypozyczenie -> next = *h_wypozyczenie;
+    *h_wypozyczenie = nowe_wypozyczenie;
 }
 
 void wyczysc_pamiec(Klient *head)
@@ -233,7 +233,53 @@ Sprzet *dodaj_sprzet(Sprzet *head)
     return nowy_sprzet;
 }
 
+//co 4 lata przestepny, co 100 lat skreslamy przestepny bo dzien trwa ~365,25dni, wyjatek co 400 lat kiedy blad arytmetyczny czasu gregorianskiego sie wyrownuje
+//uwaga! jak obliczenia sa ok to strata to 1 dzien co 3300 lat
+bool czy_przystepny_rok(int rok)
+{
+    if ((rok % 4 == 0 && rok % 100 != 0) || (rok % 400 == 0))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
+int oblicz_dni_od_zera(Data d)
+{
+    int dni_zwykly[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int dni_przestepny[] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    long suma_dni = 0;
+
+    for (int i = 1; i < d.rok; i++)
+    {
+        if(czy_przystepny_rok(i))
+        {
+            suma_dni += 366;
+        }
+        else
+        {
+            suma_dni += 365;
+        }
+
+    }
+    int *dni_miesiaca;
+    if (czy_przystepny_rok(d.rok))
+    {
+        dni_miesiaca = dni_przestepny;
+    }
+    else
+    {
+        dni_miesiaca = dni_zwykly;
+    }
+    for (int m = 0; m < dni_miesiaca-1; m++)
+    {
+        
+    }
+}
 
 int main()
 {
