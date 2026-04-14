b37fdaabbdf8b9588a94a97760b4c64460a47f77
Kordian <kdziedzic248@gmail.com>
2026-04-14 23:59:28 +0200

    Prototype v4-w. Refactor database file handling: update client and archive data saving functions, add equipment saving, and improve memory management

--- main.c
+++ main.c
@@ -103,8 +103,8 @@ void wyswietlanie_klientow(Klient *head)
     }
 }
 
-//zapis w pliku albo tworzenie jak nie ma go. robie pod windows i unix/mac. do edycji jeszcze, nie skonczone
-bool zapis(Klient *head)
+//zapis w pliku bazy danych klientow
+bool zapis_klienci(Klient *head)
 {
     const char *sciezka = "data";
     Klient *obecny = head;
@@ -114,7 +114,7 @@ bool zapis(Klient *head)
         mkdir(sciezka, 0777);
     #endif
     FILE *zapis;
-    zapis = fopen("data/database.bin", "wb");
+    zapis = fopen("data/database_clients.bin", "wb");
     if (zapis != NULL)
     {
         while(obecny != NULL)
@@ -130,11 +130,97 @@ bool zapis(Klient *head)
         return false;
     }
 }
-//wczytywanie bazy danych z pliku do RAM
-Klient *wczytaj_baze()
+
+//zapis w pliku archiwum poprzednich ofert(zakonczonych)
+bool zapis_archiwum(Wypozyczenie *head_archiwum)
 {
-    FILE *plik = fopen("data/database.bin", "rb");
+    const char *sciezka = "data";
+    Wypozyczenie *obecne = head_archiwum;
+    #ifdef _WIN32
+        _mkdir(sciezka);
+    #else 
+        mkdir(sciezka, 0777);
+    #endif
+    FILE *zapis;
+    zapis = fopen("data/database_archive.bin", "wb");
+    if (zapis != NULL)
+    {
+        while(obecne != NULL)
+        {    
+            fwrite(obecne, sizeof(Wypozyczenie), 1, zapis);
+            obecne = obecne->next;
+        }
+        fclose(zapis);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+//zapis w pliku bazy danych aktywnych ofert
+bool zapis_oferty(Wypozyczenie *head_wypozyczenia)
+{
+    const char *sciezka = "data";
+    Wypozyczenie *obecny = head_wypozyczenia;
+    #ifdef _WIN32
+        _mkdir(sciezka);
+    #else 
+        mkdir(sciezka, 0777);
+    #endif
+    FILE *zapis;
+    zapis = fopen("data/database_offers.bin", "wb");
+    if (zapis != NULL)
+    {
+        while(obecny != NULL)
+        {    
+            fwrite(obecny, sizeof(Wypozyczenie), 1, zapis);
+            obecny = obecny->next;
+        }
+        fclose(zapis);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+//zapis w pliku bazy danych sprzetu
+bool zapis_sprzet(Sprzet *head_sprzet)
+{
+    const char *sciezka = "data";
+    Sprzet *obecny = head_sprzet;
+    #ifdef _WIN32
+        _mkdir(sciezka);
+    #else 
+        mkdir(sciezka, 0777);
+    #endif
+    FILE *zapis;
+    zapis = fopen("data/database_sprzet.bin", "wb");
+    if (zapis != NULL)
+    {
+        while(obecny != NULL)
+        {    
+            fwrite(obecny, sizeof(Sprzet), 1, zapis);
+            obecny = obecny->next;
+        }
+        fclose(zapis);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+//wczytywanie bazy danych klientow z pliku do RAM
+Klient *wczytaj_baze_klientow()
+{
+    FILE *plik = fopen("data/database_clients.bin", "rb");
     Klient *head = NULL;
+    Klient *ostatni = NULL; //dodalem aby moc "obrocic" liste, bo tak to wczyta sie do ramu do gory nogami
     Klient temp;
 
     if (plik != NULL)
@@ -143,98 +229,120 @@ Klient *wczytaj_baze()
         {
             Klient *nowy = malloc(sizeof(Klient));
             *nowy = temp;
-            nowy->next = head;
-            head = nowy;
+            nowy->next = NULL;
+            
+            if(head == NULL)
+            {
+                head = nowy;
+                ostatni = nowy;
+            }
+            else
+            {
+                ostatni->next = nowy;
+                ostatni = nowy;
+            }
         }
-
+        fclose(plik);
     }
     return head;
 }
 
-void dodaj_wypozyczenie(Klient **h_klienci, Sprzet **h_sprzet, Wypozyczenie **h_wypozyczenie)
+//wczytywanie bazy danych offert z pliku do RAM
+Wypozyczenie *wczytaj_baze_ofert()
 {
-    int szukana_karta, szukany_sprzet;
-    Klient *obecny_klient = *h_klienci;
-    while(obecny_klient != NULL && obecny_klient->numer_karty != szukana_karta)
-    {
-        obecny_klient = obecny_klient->next;
+    FILE *plik = fopen("data/database_offers.bin", "rb");
+    Wypozyczenie *head = NULL;
+    Wypozyczenie *ostatni = NULL; //dodalem aby moc "obrocic" liste, bo tak to wczyta sie do ramu do gory nogami
+    Wypozyczenie temp;
 
-    }
-    if (obecny_klient == NULL)
+    if (plik != NULL)
     {
-        printf("Blad. Nie znaleziono klienta!\n");
-        return;
+        while (fread(&temp, sizeof(Wypozyczenie), 1, plik) == 1)
+        {
+            Wypozyczenie *nowy = malloc(sizeof(Wypozyczenie));
+            *nowy = temp;
+            nowy->next = NULL;
+            
+            if(head == NULL)
+            {
+                head = nowy;
+                ostatni = nowy;
+            }
+            else
+            {
+                ostatni->next = nowy;
+                ostatni = nowy;
+            }
+        }
+        fclose(plik);
     }
-    Sprzet *obecny_sprzet = *h_sprzet;
-    while(obecny_sprzet != NULL && obecny_sprzet->id_sprzetu != szukany_sprzet)
-    {
-        obecny_sprzet = obecny_sprzet->next;
-    }
-    if (obecny_sprzet == NULL)
-    {
-        printf("Blad. Nie znaleziono sprzetu!\n");
-        return;
-    }
-    if (obecny_sprzet->liczba_wypozyczonych >= obecny_sprzet->liczba_egzemplarzy)
-    {
-        printf("Blad. Brak wolnego egzemplarza do wypozyczenia!\n");
-        return;
-    }
-    Wypozyczenie *nowe_wypozyczenie = malloc(sizeof(Wypozyczenie));
-    if (nowe_wypozyczenie == NULL)
-    {
-        printf("Blad. Brak pamieci RAM!\n");
-        return;
-    }
-    nowe_wypozyczenie -> numer_karty = szukana_karta;
-    nowe_wypozyczenie -> id_sprzetu = szukany_sprzet;
-    obecny_sprzet -> liczba_wypozyczonych ++;
-    nowe_wypozyczenie -> next = *h_wypozyczenie;
-    *h_wypozyczenie = nowe_wypozyczenie;
+    return head;
 }
 
-void wyczysc_pamiec(Klient *head)
+//wczytywanie bazy danych archiwum z pliku archiwum do RAM
+Wypozyczenie *wczytaj_archiwum()
 {
-    Klient *obecny = head;
-    Klient *tmp;
-    while (obecny != NULL)
+    FILE *plik = fopen("data/database_archive.bin", "rb");
+    Wypozyczenie *head = NULL;
+    Wypozyczenie *ostatni = NULL; //dodalem aby moc "obrocic" liste, bo tak to wczyta sie do ramu do gory nogami
+    Wypozyczenie temp;
+
+    if (plik != NULL)
     {
-        tmp = obecny -> next;
-        free(obecny);
-        obecny = tmp;
+        while (fread(&temp, sizeof(Wypozyczenie), 1, plik) == 1)
+        {
+            Wypozyczenie *nowy = malloc(sizeof(Wypozyczenie));
+            *nowy = temp;
+            nowy->next = NULL;
+            
+            if(head == NULL)
+            {
+                head = nowy;
+                ostatni = nowy;
+            }
+            else
+            {
+                ostatni->next = nowy;
+                ostatni = nowy;
+            }
+        }
+        fclose(plik);
     }
+    return head;
 }
 
-Sprzet *dodaj_sprzet(Sprzet *head)
+//wzczytywanie bazy danych sprzetu z pliku do RAMu
+Sprzet *wczytaj_sprzet()
 {
-    Sprzet *nowy_sprzet = malloc(sizeof(Sprzet));
-    if (nowy_sprzet == NULL)
+    FILE *plik = fopen("data/database_sprzet.bin", "rb");
+    Sprzet *head = NULL;
+    Sprzet *ostatni = NULL; //dodalem aby moc "obrocic" liste, bo tak to wczyta sie do ramu do gory nogami
+    Sprzet temp;
+
+    if (plik != NULL)
     {
-        printf("Blad. Brak pamieci RAM!\n");
-        return head;
+        while (fread(&temp, sizeof(Sprzet), 1, plik) == 1)
+        {
+            Sprzet *nowy = malloc(sizeof(Sprzet));
+            *nowy = temp;
+            nowy->next = NULL;
+            
+            if(head == NULL)
+            {
+                head = nowy;
+                ostatni = nowy;
+            }
+            else
+            {
+                ostatni->next = nowy;
+                ostatni = nowy;
+            }
+        }
+        fclose(plik);
     }
-    //ponownie, generowanie numeru ID by bylo pomocne, ale zostawiam narazie wpisywanie reczne
-    printf("Podaj ID sprzetu.\n");
-    scanf("%d", &nowy_sprzet->id_sprzetu);
-
-    printf("Podaj nazwe sprzetu.\n");
-    scanf("%s", nowy_sprzet->nazwa_sprzetu);
-
-    printf("Podaj cene sprzetu za dzien.\n");
-    scanf("%lf", &nowy_sprzet->cena_za_dzien);
-
-    printf("Podaj liczbe egzemplarzy.\n");
-    scanf("%d", &nowy_sprzet->liczba_egzemplarzy);
-
-    printf("Podaj liczbe wypozyczonych egzemplarzy (Domyslnie nowy sprzet - 0)\n");
-    scanf("%d", &nowy_sprzet->liczba_wypozyczonych);
-    
-    nowy_sprzet->next = head;
-    return nowy_sprzet;
+    return head;
 }
 
-//co 4 lata przestepny, co 100 lat skreslamy przestepny bo dzien trwa ~365,25dni, wyjatek co 400 lat kiedy blad arytmetyczny czasu gregorianskiego sie wyrownuje
-//uwaga! jak obliczenia sa ok to strata to 1 dzien co 3300 lat
 bool czy_przystepny_rok(int rok)
 {
     if ((rok % 4 == 0 && rok % 100 != 0) || (rok % 400 == 0))
@@ -275,27 +383,245 @@ int oblicz_dni_od_zera(Data d)
     {
         dni_miesiaca = dni_zwykly;
     }
-    for (int m = 0; m < dni_miesiaca-1; m++)
+    for (int m = 0; m < d.miesiac - 1; m++)
     {
-        
+        suma_dni = dni_miesiaca[m] + suma_dni;
+    }
+    suma_dni = suma_dni + d.dzien;
+    return suma_dni;
+}
+
+void dodaj_wypozyczenie(Klient **h_klienci, Sprzet **h_sprzet, Wypozyczenie **h_wypozyczenie)
+{
+    int szukana_karta, szukany_sprzet;
+    printf("Podaj numer karty klienta.\n");
+    scanf("%d", &szukana_karta);
+    printf("Podaj ID sprzetu do wypozyczenia.\n");
+    scanf("%d", &szukany_sprzet);
+    
+
+    Klient *obecny_klient = *h_klienci;
+    while(obecny_klient != NULL && obecny_klient->numer_karty != szukana_karta)
+    {
+        obecny_klient = obecny_klient->next;
+
+    }
+    if (obecny_klient == NULL)
+    {
+        printf("Blad. Nie znaleziono klienta!\n");
+        return;
+    }
+    Sprzet *obecny_sprzet = *h_sprzet;
+    while(obecny_sprzet != NULL && obecny_sprzet->id_sprzetu != szukany_sprzet)
+    {
+        obecny_sprzet = obecny_sprzet->next;
+    }
+    if (obecny_sprzet == NULL)
+    {
+        printf("Blad. Nie znaleziono sprzetu!\n");
+        return;
+    }
+    if (obecny_sprzet->liczba_wypozyczonych >= obecny_sprzet->liczba_egzemplarzy)
+    {
+        printf("Blad. Brak wolnego egzemplarza do wypozyczenia!\n");
+        return;
+    }
+    Wypozyczenie *nowe_wypozyczenie = malloc(sizeof(Wypozyczenie));
+    if (nowe_wypozyczenie == NULL)
+    {
+        printf("Blad. Brak pamieci RAM!\n");
+        return;
+    }
+    printf("Podaj dzien wypozyczenia.\n");
+    scanf("%d", &nowe_wypozyczenie->data_wypozyczenia.dzien);
+    printf("Podaj miesiac wypozyczenia.\n");
+    scanf("%d", &nowe_wypozyczenie->data_wypozyczenia.miesiac);
+    printf("Podaj rok wypozyczenia.\n");
+    scanf("%d", &nowe_wypozyczenie->data_wypozyczenia.rok);
+
+    printf("Podaj umowny dzien oddania.\n");
+    scanf("%d", &nowe_wypozyczenie->przewidywana_data_oddania.dzien);
+    printf("Podaj umowny miesiac oddania.\n");
+    scanf("%d", &nowe_wypozyczenie->przewidywana_data_oddania.miesiac);
+    printf("Podaj umowny rok oddania.\n");
+    scanf("%d", &nowe_wypozyczenie->przewidywana_data_oddania.rok);
+
+    nowe_wypozyczenie->realna_data_oddania.dzien = 0;
+    nowe_wypozyczenie->realna_data_oddania.miesiac = 0;
+    nowe_wypozyczenie->realna_data_oddania.rok = 0;
+    printf("Uwaga! 'realna data oddania' jest automatycznie ustawiana na 0. Nalezy ja edytowac po oddaniu przez klienta sprzetu z powrotem do sklepu. Kara za spoznienie bedzie naliczana pozniej.\n ");
+
+    int dni_wypozyczenia = (oblicz_dni_od_zera(nowe_wypozyczenie->przewidywana_data_oddania) - oblicz_dni_od_zera(nowe_wypozyczenie->data_wypozyczenia));
+    nowe_wypozyczenie->oplacona_kwota = dni_wypozyczenia * obecny_sprzet->cena_za_dzien;
+    nowe_wypozyczenie -> numer_karty = szukana_karta;
+    nowe_wypozyczenie -> id_sprzetu = szukany_sprzet;
+    obecny_sprzet -> liczba_wypozyczonych ++;
+    nowe_wypozyczenie -> next = *h_wypozyczenie;
+    *h_wypozyczenie = nowe_wypozyczenie;
+}
+
+void zwroc_sprzet(Klient **hz_klienci, Sprzet **hz_sprzet, Wypozyczenie **hz_wypozyczenie, Wypozyczenie **hz_archiwum)
+{
+    int szukana_karta, szukany_sprzet;
+    printf("Podaj numer karty.\n");
+    scanf("%d", &szukana_karta);
+    printf("Podaj ID sprzetu.\n");
+    scanf("%d", &szukany_sprzet);
+    Wypozyczenie *zwrot_obecne_wypozyczenie = *hz_wypozyczenie;
+    Sprzet *zwrot_sprzet = *hz_sprzet;
+    Wypozyczenie *poprzednie_wypozyczenie = NULL;
+    while (zwrot_obecne_wypozyczenie != NULL && (szukana_karta != zwrot_obecne_wypozyczenie->numer_karty || szukany_sprzet != zwrot_obecne_wypozyczenie->id_sprzetu))
+    {
+        poprzednie_wypozyczenie = zwrot_obecne_wypozyczenie; 
+        zwrot_obecne_wypozyczenie = zwrot_obecne_wypozyczenie->next;
+    }
+    if (zwrot_obecne_wypozyczenie == NULL)
+    {
+        printf("Umowa o podanych parametrach nie istnieje!\n");
+        return;
+    }
+    if (poprzednie_wypozyczenie == NULL)
+    {
+        *hz_wypozyczenie = zwrot_obecne_wypozyczenie->next;
+    }
+    else
+    {
+        poprzednie_wypozyczenie->next = zwrot_obecne_wypozyczenie->next;
+    }
+    while (zwrot_sprzet != NULL && szukany_sprzet != zwrot_sprzet->id_sprzetu)
+    {
+        zwrot_sprzet = zwrot_sprzet->next;
+    }
+    if (zwrot_sprzet == NULL)
+    {
+        printf("Blad bazy sprzetu!");
+        return;
+    }
+    zwrot_obecne_wypozyczenie -> next = *hz_archiwum;
+    *hz_archiwum = zwrot_obecne_wypozyczenie;
+
+    printf("Kiedy nastapilo oddanie sprzetu? (dzien)");
+    scanf("%d", &zwrot_obecne_wypozyczenie->realna_data_oddania.dzien);
+    printf("Kiedy nastapilo oddanie sprzetu? (miesiac)");
+    scanf("%d", &zwrot_obecne_wypozyczenie->realna_data_oddania.miesiac);
+    printf("Kiedy nastapilo oddanie sprzetu? (rok)");
+    scanf("%d", &zwrot_obecne_wypozyczenie->realna_data_oddania.rok);
+
+    int dni_spoznienia = oblicz_dni_od_zera(zwrot_obecne_wypozyczenie->realna_data_oddania) - oblicz_dni_od_zera(zwrot_obecne_wypozyczenie->przewidywana_data_oddania);
+    double cena_dzienna_opoznienie = zwrot_sprzet->cena_za_dzien;
+    double kara_opoznienie = dni_spoznienia*cena_dzienna_opoznienie*0.2;
+    if (dni_spoznienia <= 0)
+    {
+        kara_opoznienie = 0;
+        printf("Brak kary za opoznienie. Sprzet oddany na czas.\n");
+    }
+    else
+    {
+        printf("Kara za opoznienia wynosi %.2f: \n", kara_opoznienie);
+    }
+    zwrot_sprzet->liczba_wypozyczonych--;
+    return;
+}
+// czyszczenie pamieci ram
+void wyczysc_pamiec_klient(Klient *head)
+{
+    Klient *obecny = head;
+    Klient *tmp;
+    while (obecny != NULL)
+    {
+        tmp = obecny -> next;
+        free(obecny);
+        obecny = tmp;
     }
 }
+
+void wyczysc_pamiec_sprzet(Sprzet *head)
+{
+    Sprzet *obecny = head;
+    Sprzet *tmp;
+    while (obecny != NULL)
+    {
+        tmp = obecny -> next;
+        free(obecny);
+        obecny = tmp;
+    }
+}
+
+void wyczysc_pamiec_archiwum(Wypozyczenie *head)
+{
+    Wypozyczenie *obecny = head;
+    Wypozyczenie *tmp;
+    while (obecny != NULL)
+    {
+        tmp = obecny -> next;
+        free(obecny);
+        obecny = tmp;
+    }
+}
+
+void wyczysc_pamiec_oferty(Wypozyczenie *head)
+{
+    Wypozyczenie *obecny = head;
+    Wypozyczenie *tmp;
+    while (obecny != NULL)
+    {
+        tmp = obecny -> next;
+        free(obecny);
+        obecny = tmp;
+    }
+}
+
+Sprzet *dodaj_sprzet(Sprzet *head)
+{
+    Sprzet *nowy_sprzet = malloc(sizeof(Sprzet));
+    if (nowy_sprzet == NULL)
+    {
+        printf("Blad. Brak pamieci RAM!\n");
+        return head;
+    }
+    //ponownie, generowanie numeru ID by bylo pomocne, ale zostawiam narazie wpisywanie reczne
+    printf("Podaj ID sprzetu.\n");
+    scanf("%d", &nowy_sprzet->id_sprzetu);
+
+    printf("Podaj nazwe sprzetu.\n");
+    scanf("%s", nowy_sprzet->nazwa_sprzetu);
+
+    printf("Podaj cene sprzetu za dzien.\n");
+    scanf("%lf", &nowy_sprzet->cena_za_dzien);
+
+    printf("Podaj liczbe egzemplarzy.\n");
+    scanf("%d", &nowy_sprzet->liczba_egzemplarzy);
+
+    printf("Podaj liczbe wypozyczonych egzemplarzy (Domyslnie nowy sprzet - 0)\n");
+    scanf("%d", &nowy_sprzet->liczba_wypozyczonych);
+    
+    nowy_sprzet->next = head;
+    return nowy_sprzet;
+}
+
+//co 4 lata przestepny, co 100 lat skreslamy przestepny bo dzien trwa ~365,25dni, wyjatek co 400 lat kiedy blad arytmetyczny czasu gregorianskiego sie wyrownuje
+//uwaga! jak obliczenia sa ok to strata to 1 dzien co 3300 lat
+
 
 int main()
 {
     //heady
-    Klient *head_klienci = wczytaj_baze();
-    Sprzet *head_sprzet = NULL;
-    Wypozyczenie *head_wypozyczenia = NULL;
+    Klient *head_klienci = wczytaj_baze_klientow();
+    Sprzet *head_sprzet = wczytaj_sprzet();
+    Wypozyczenie *head_wypozyczenia = wczytaj_baze_ofert();
+    Wypozyczenie *head_archiwum = wczytaj_archiwum();
     
-    //main sys nieskonczony
+    //main sys 
     while(1)
     {
         int opcja = 0;
         printf("Witamy. Wybierz opcję do administracji systemu wypożyczania nart.\n");
         printf("1. Wyświetl bazę danych.\n");
         printf("2. Dodaj klienta.\n");
-        printf("3. Zamknij program.\n");
+        printf("3. Dodaj sprzet do inwentarza.\n");
+        printf("4. Podpisz nowy kontrakt.\n");
+        printf("5. Zwrot sprzetu (oddanie).\n");
+        printf("6. BEZPIECZNE zamkniecie programu.\n");
         if (scanf("%d", &opcja) != 1)
         {
             while(getchar() != '\n');
@@ -305,25 +631,50 @@ int main()
         {
             switch (opcja)
             {
-            case 1:
+                case 1:
                 {
                     printf("Wyświetlam bazę danych.\n");
                     break;
                 }
-            case 2:
+                case 2:
                 {
                     printf("Rozpoczynam dodawanie klienta do bazy danych.\n");
                     head_klienci = dodaj_klienta(head_klienci);
-
                     break;
                 }
-            case 3:
+                case 3:
                 {
-                    printf("Zamykanie...\n");
-                    wyczysc_baze(head_klienci);
+                    printf("Dodawanie sprzetu do inwentarza.\n");
+                    dodaj_sprzet(head_sprzet);
+                    break;
+                }
+                case 4:
+                {
+                    printf("Podpisanie kontraktu (wypozyczenie sprzetu np. nart)");
+                    dodaj_wypozyczenie(&head_klienci, &head_sprzet, &head_wypozyczenia);
+                    break;
+                }
+                case 5:
+                {
+                    printf("Zwrot sprzetu(+ew naliczenie kary)");
+                    zwroc_sprzet(&head_klienci, &head_sprzet, &head_wypozyczenia, &head_archiwum);
+                    break;
+                }
+                case 6:
+                {
+                    printf("Zamykanie... Zaczekaj do konca aby uniknac utraty danych!\n");
+                    zapis_sprzet(head_sprzet);
+                    zapis_archiwum(head_archiwum);
+                    zapis_klienci(head_klienci);
+                    zapis_oferty(head_wypozyczenia);
+                    wyczysc_pamiec_archiwum(head_archiwum);
+                    wyczysc_pamiec_klient(head_klienci);
+                    wyczysc_pamiec_oferty(head_wypozyczenia);
+                    wyczysc_pamiec_sprzet(head_sprzet);
+                    printf("Bezpiecznie zapisano i zamknieto program.\n");
                     return 0;
                 }
-            default:
+                default:
                 {
                     printf("Wybrano nieprawidłową opcję. Spróbuj ponownie.");
                     break;   
